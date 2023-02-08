8a604ad654f8b79a670197d5fa9c4faee36ef679
Sergii79 <tsymbal.sergiy@gmail.com>
2023-02-08 16:11:09 +0200

    commit fivth

--- js/task-08.js
+++ js/task-08.js
@@ -1,71 +1,46 @@
-const form = document.querySelector(".login-form");
-const data = {
-    email: '',
-    password: ''
-};
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+//* 1-й варіант:
+
+
+const form = document.querySelector('.login-form');
+console.log(form);
+
+function handleSubmit(event) {
+    event.preventDefault();
     const {
         elements: { email, password }
-    } = e.currentTarget;
-
-    if (email.value === '' || password.value === '') {
-        return alert('All fields are required!');
+    } = event.currentTarget;
+    
+    if (email.value === "" || password.value === "") {
+        return alert("Please fill in all the fields!");
     }
+    
+    console.log(`email: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+}
 
-    data.email = email.value;
-    data.password = password.value;
-    console.log(data);
-    e.currentTarget.reset();
-});
-
-
+form.addEventListener('submit', handleSubmit);
 
 
+//* 2-й варіант:
 
 
-
-// const form = document.querySelector('.login-form');
-// console.log(form);
-
-// function handleSubmit(event) {
+// const form = document.querySelector(".login-form");
+// const user = {
+//     email: '',
+//     password: ''
+// };
+// form.addEventListener('submit', (event) => {
 //     event.preventDefault();
 //     const {
-//         elements: { login, password }
+//         elements: { email, password }
 //     } = event.currentTarget;
-    
-//     if (login.value === "" || password.value === "") {
-//         return alert("Please fill in all the fields!");
+
+//     if (email.value === '' || password.value === '') {
+//         return alert('All fields are required!');
 //     }
-    
-//     console.log(`Login: ${login.value}, Password: ${password.value}`);
+
+//     user.email = email.value;
+//     user.password = password.value;
+//     console.log(data);
 //     event.currentTarget.reset();
-// }
-
-// form.addEventListener('submit', handleSubmit);
-
-
-
-
-
-
-
-
-// const form = document.querySelector('.login-form');
-
-// function handleSubmit(event) {
-//     event.preventDefault();
-//     const {
-//         elements: { login, password }
-//     } = event.currentTarget;
-    
-//     if (login.value.trim === "" || password.value === "") {
-//         return alert("Please fill in all the fields!");
-//     }
-    
-//     const userDetails = { email: email.value, password: password.value }
-//     console.log(userDetails);
-//     event.correntTarget.reset();
-// }
-
-// form.addEventListener('submit', handleSubmit);
+// });
--- js/task-10.js
+++ js/task-10.js
@@ -1,60 +1,40 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-
-// const div = document.getElementById('controls');
-
-// const input = document.querySelector('input[type="number"]');
-
-// const buttonCreate = document.querySelector('button[data-create]');
-
-// const buttonDestroy = document.querySelector('button[data-destroy]');
-
-
-// function createBoxes(amount) {
-
-// }
-
-// function buttonCreateChange() {
-
-// }
-
-
-
-// buttonCreate.addEventListener('click', buttonCreateChange);
-
-// buttonDestroy.addEventListener('click', buttonDestroyChange);
-
-
-
-
-
-const inputNum = document.querySelector('#controls>input')
-const btnCreate = document.querySelector('button[data-create]')
-const btnDestroy = document.querySelector('button[data-destroy]')
-const boxes = document.querySelector('#boxes')
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const createBoxes = amount => {
-    const elementsToAdd = []
+
+
+const input = document.querySelector('input[type="number"]');
+
+const buttonCreate = document.querySelector('button[data-create]');
+
+const buttonDestroy = document.querySelector('button[data-destroy]');
+
+const boxes = document.getElementById('boxes');
+
+
+function createBoxes(amount) {
+    const createElements = []
     for (let i = 0; i < amount; i++) {
         const div = document.createElement('div')
-        div.style.height = `${30 + 10 * i}px`
-        div.style.width = `${30 + 10 * i}px`
+        div.style.height = `${25 + 5 * i}px`
+        div.style.width = `${50 + 10 * i}px`
         div.style.background = getRandomHexColor()
-        elementsToAdd.push(div)
+        createElements.push(div)
     }
-    return elementsToAdd
+    return createElements
 }
+
+
 const destroyBoxes = () => {
     boxes.innerHTML = ''
 }
-btnCreate.addEventListener('click', () => {
-    let boxesToAdd = createBoxes(inputNum.value)
+
+ buttonCreate.addEventListener('click', () => {
+    const boxesToAdd = createBoxes(input.value)
     boxes.append(...boxesToAdd)
-})
-console.log(inputNum.value)
-btnDestroy.addEventListener('click', () => {
+ })
+
+ buttonDestroy.addEventListener('click', () => {
     destroyBoxes.call()
 })
+
